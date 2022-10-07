d82bda70c4c7ce107d5f998291a17b38a4a17654
takuya <55338+takuya@users.noreply.github.com>
2022-10-07 17:23:13 +0900

    fix-symlink

--- src/sogo-patch.sh
+++ src/sogo-patch.sh
@@ -7,3 +7,9 @@ cd $sogo_dir
 
 sed  -i  '/keys\.openpgp\.org/,+6 d' Dockerfile 
 grep arm64 Dockerfile > /dev/null || sed -i "/locale/r${f}" Dockerfile
+
+
+
+sed -i '/rsync -a \/usr/c rsync -avL /usr/lib/GNUstep/SOGo/. /sogo_web/' bootstrap-sogo.sh
+
+
